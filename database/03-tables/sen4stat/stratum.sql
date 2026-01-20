6979e8b8ec7caeaee66d74a32bc8d6881b9a0936
Laurențiu Nicola <lnicola@dend.ro>
2026-01-20 10:45:46 +0200

    Fix season_id column in stratum table

--- database/03-tables/sen4stat/stratum.sql
+++ database/03-tables/sen4stat/stratum.sql
@@ -1,8 +1,8 @@
 create table stratum(
     site_id smallint not null,
-    year smallint not null,
+    season_id smallint not null,
     stratum_type_id smallint not null,
     stratum_id int not null,
     wkb_geometry geometry not null,
-    primary key (site_id, year, stratum_type_id, stratum_id)
+    primary key (site_id, season_id, stratum_type_id, stratum_id)
 );
