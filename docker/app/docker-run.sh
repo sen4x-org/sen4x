024cee9c2c38d5930d9683fc3a84983e0aa44516
Laurențiu Nicola <lnicola@dend.ro>
2026-07-13 15:36:23 +0300

    Drop chown in app docker-run

--- docker/app/docker-run.sh
+++ docker/app/docker-run.sh
@@ -6,6 +6,5 @@ docker build \
 docker run -it --rm \
        -v $PWD/../..:/sen4x:z \
        -v $PWD/entry.sh:/entry.sh:z \
+       -u $(id -u):$(id -g) \
        sen4x-app-build /entry.sh
-
-sudo chown $USER:$USER ../../packaging/Sen2AgriRPM/sen2agri-app-*.rpm
