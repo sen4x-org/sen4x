32d799c1bcbd09f73d99139b4a6ff9799cf865ce
Laurențiu Nicola <lnicola@dend.ro>
2026-07-13 15:39:31 +0300

    Don't try to allocate a tty

--- docker/app/docker-run.sh
+++ docker/app/docker-run.sh
@@ -3,7 +3,7 @@ docker build \
     -t sen4x-app-build \
     .
 
-docker run -it --rm \
+docker run -i --rm \
        -v $PWD/../..:/sen4x:z \
        -v $PWD/entry.sh:/entry.sh:z \
        -u $(id -u):$(id -g) \
