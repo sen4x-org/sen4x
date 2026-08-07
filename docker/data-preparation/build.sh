1f4ea1e37224570d7a3ed3eb045777f26e3d9125
Laurențiu Nicola <lnicola@dend.ro>
2026-08-07 08:59:50 +0300

    Base data-preparation image on erdy

--- docker/data-preparation/build.sh
+++ docker/data-preparation/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-tar -czh . | docker build -t sen4x/data-preparation:0.4 -
-# docker build -f Dockerfile.isect -t sen4cap/data-preparation:0.4-isect .
+tar -czh . | docker build -t sen4x/data-preparation:0.5 -
