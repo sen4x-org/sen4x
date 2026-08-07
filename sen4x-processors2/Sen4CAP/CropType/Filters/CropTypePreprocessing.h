625ee287d23a9731ae73583eab83cd7e6e5525e1
Laurențiu Nicola <lnicola@dend.ro>
2026-08-07 17:21:33 +0300

    Port Sen4CAP crop type

---
+++ sen4x-processors2/Sen4CAP/CropType/Filters/CropTypePreprocessing.h
@@ -0,0 +1,174 @@
+/*=========================================================================
+  *
+  * Program:      Sen2agri-Processors
+  * Language:     C++
+  * Copyright:    2015-2016, CS Romania, office@c-s.ro
+  * See COPYRIGHT file for details.
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+
+ =========================================================================*/
+
+#pragma once
+
+#include "otbWrapperApplication.h"
+#include "otbWrapperApplicationFactory.h"
+#include "otbWrapperApplicationRegistry.h"
+#include "otbWrapperInputImageListParameter.h"
+
+#include "otbImageList.h"
+#include "otbVectorImage.h"
+
+#include "otbStreamingResampleImageFilter.h"
+
+// Transform
+#include "itkScalableAffineTransform.h"
+#include "itkScaleTransform.h"
+
+// Filters
+#include "otbMultiChannelExtractROI.h"
+
+#include "otbCropTypeFeatureExtractionFilter.h"
+#include "otbTemporalResamplingFilter.h"
+
+#include "TimeSeriesReader.h"
+
+#include <string>
+
+typedef otb::TemporalResamplingFilter<ImageType, MaskType, ImageType> TemporalResamplingFilterType;
+typedef otb::ObjectList<TemporalResamplingFilterType> TemporalResamplingFilterListType;
+
+typedef otb::CropTypeFeatureExtractionFilter<ImageType> CropTypeFeatureExtractionFilterType;
+typedef otb::ObjectList<CropTypeFeatureExtractionFilterType>
+    CropTypeFeatureExtractionFilterListType;
+class CropTypePreprocessing : public TimeSeriesReader
+{
+public:
+    typedef CropTypePreprocessing Self;
+    typedef TimeSeriesReader Superclass;
+    typedef itk::SmartPointer<Self> Pointer;
+    typedef itk::SmartPointer<const Self> ConstPointer;
+
+    itkNewMacro(Self);
+    itkTypeMacro(CropTypePreprocessing, TimeSeriesReader);
+
+    itkSetMacro(IncludeRedEdge, bool);
+    itkGetMacro(IncludeRedEdge, bool);
+    itkSetMacro(UseSwir2Band, bool);
+    itkGetMacro(UseSwir2Band, bool);
+
+    CropTypePreprocessing() : m_IncludeRedEdge(), m_UseSwir2Band()
+    {
+        m_TemporalResamplers = TemporalResamplingFilterListType::New();
+        m_FeatureExtractors = CropTypeFeatureExtractionFilterListType::New();
+    }
+
+    void getRedEdgeBands(const std::unique_ptr<MetadataHelper<float, uint8_t>> &pHelper,
+                         const TileData &td,
+                         ImageDescriptor &descriptor) override
+    {
+        if (!m_IncludeRedEdge) {
+            return;
+        }
+        std::vector<std::string> redEdgeBands = pHelper->GetRedEdgeBandNames();
+        if (redEdgeBands.size() == 0) {
+            return;
+        }
+        if (this->m_UseSwir2Band) {
+            const std::string &swir2BandName = pHelper->GetSwir2BandName();
+            if (swir2BandName.size() == 0) {
+                return;
+            }
+            redEdgeBands.push_back(swir2BandName);
+        } else {
+            const std::string &narrowNirBandName = pHelper->GetNarrowNirBandName();
+            if (narrowNirBandName.size() == 0) {
+                return;
+            }
+            redEdgeBands.push_back(narrowNirBandName);
+        }
+
+        ExtractFloatChannelFilterType::Pointer channelExtractor;
+
+        std::vector<int> relBandsIdxs;
+        MetadataHelper<float, uint8_t>::VectorImageType::Pointer img =
+            pHelper->GetImage(redEdgeBands, &relBandsIdxs);
+        img->UpdateOutputInformation();
+
+        for (int bandIndex : relBandsIdxs) {
+            channelExtractor = ExtractFloatChannelFilterType::New();
+            channelExtractor->SetInput(img);
+            channelExtractor->SetIndex(bandIndex);
+            m_Filters->PushBack(channelExtractor);
+            auto resampledBand =
+                getResampledBand<FloatImageType>(channelExtractor->GetOutput(), td, false);
+            descriptor.bands.push_back(resampledBand);
+        }
+    }
+
+    int getBandCount(const std::string &sensor) override
+    {
+        if (sensor == "SENTINEL") {
+            if (m_IncludeRedEdge) {
+                return 8;
+            } else {
+                return 4;
+            }
+        } else if (sensor == "SPOT" || sensor == "LANDSAT") {
+            return 4;
+        }
+        itkGenericExceptionMacro("Unknown sensor " << sensor);
+    }
+
+    otb::Wrapper::FloatVectorImageType *GetOutput(const std::vector<MissionDays> &sensorOutDays)
+    {
+        auto temporalResampler = TemporalResamplingFilterType::New();
+        auto featureExtractor = CropTypeFeatureExtractionFilterType::New();
+
+        m_TemporalResamplers->PushBack(temporalResampler);
+        m_FeatureExtractors->PushBack(featureExtractor);
+
+        otb::SensorDataCollection sdCollection;
+        for (const auto &e : sensorOutDays) {
+            otb::SensorData sd;
+
+            sd.sensorName = e.mission;
+
+            sd.outDates = e.days;
+            sd.bandCount = this->getBandCount(e.mission);
+
+            for (const ImageDescriptor &id : m_Descriptors) {
+                if (id.mission == e.mission) {
+                    int inDay = getDaysFromEpoch(id.aquisitionDate);
+                    sd.inDates.push_back(inDay);
+                }
+            }
+
+            sdCollection.emplace_back(sd);
+        }
+
+        // Set the temporal resampling / gap filling filter
+        temporalResampler->SetInputRaster(m_BandsConcat->GetOutput());
+        temporalResampler->SetInputMask(m_MaskConcat->GetOutput());
+        // The output days will be updated later
+        temporalResampler->SetInputData(sdCollection);
+
+        featureExtractor->SetInput(temporalResampler->GetOutput());
+        featureExtractor->SetSensorData(sdCollection);
+
+        return featureExtractor->GetOutput();
+    }
+
+private:
+    bool m_IncludeRedEdge;
+    bool m_UseSwir2Band;
+
+    TemporalResamplingFilterListType::Pointer m_TemporalResamplers;
+    CropTypeFeatureExtractionFilterListType::Pointer m_FeatureExtractors;
+};
+
+typedef otb::ObjectList<CropTypePreprocessing> CropTypePreprocessingList;
---
+++ sen4x-processors2/Sen4CAP/CropType/OpticalFeatures/OpticalFeatures.cpp
@@ -0,0 +1,282 @@
+/*=========================================================================
+  *
+  * Program:      Sen2agri-Processors
+  * Language:     C++
+  * Copyright:    2015-2016, CS Romania, office@c-s.ro
+  * See COPYRIGHT file for details.
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+
+ =========================================================================*/
+
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbWrapperApplication.h"
+#include "otbWrapperTypes.h"
+
+#include "../Filters/CropTypePreprocessing.h"
+#include "../Filters/RedEdgeBandsExtractor.h"
+#include "../Filters/otbStreamingStatisticsMapFromLabelImageFilter2.h"
+
+namespace otb
+{
+
+namespace Wrapper
+{
+class OpticalFeatures : public Application
+{
+public:
+    typedef OpticalFeatures Self;
+    typedef Application Superclass;
+    typedef itk::SmartPointer<Self> Pointer;
+    typedef itk::SmartPointer<const Self> ConstPointer;
+
+    itkNewMacro(Self);
+
+    itkTypeMacro(OpticalFeatures, otb::Application);
+
+    typedef Int16VectorImageType OutputImageType;
+    typedef FloatVectorImageType FeatureImageType;
+    typedef Int32ImageType ClassImageType;
+
+    typedef otb::StreamingStatisticsMapFromLabelImageFilter2<FeatureImageType, ClassImageType>
+        StatisticsFilterType;
+
+private:
+    void DoInit() override
+    {
+        SetName("OpticalFeatures");
+        SetDescription("Computes optical features statistics for a set of tiles");
+
+        SetDocLongDescription("Computes optical features statistics for a set of tiles.");
+        SetDocLimitations("None");
+        SetDocAuthors("LN");
+        SetDocSeeAlso(" ");
+
+        AddDocTag(Tags::Vector);
+
+        AddParameter(ParameterType_InputFilenameList, "il", "Input descriptors");
+        SetParameterDescription("il", "The list of descriptors. They must be sorted by tiles.");
+
+        AddParameter(ParameterType_InputImage, "ref", "Support image");
+        SetParameterDescription("ref", "Support image for computing statistics.");
+
+        AddParameter(ParameterType_InputFilename, "dates", "Sampling dates");
+        SetParameterDescription("dates", "The list of days on which to sample the inputs");
+        MandatoryOff("dates");
+
+        AddParameter(ParameterType_OutputFilename, "outdates", "Sampling dates");
+        SetParameterDescription("outdates", "The list of days on which to sample the inputs");
+        MandatoryOff("outdates");
+
+        AddParameter(ParameterType_OutputFilename, "outmean", "Class means");
+        SetParameterDescription("outmean", "Per-class means output CSV");
+        AddParameter(ParameterType_OutputFilename, "outdev", "Class stddev");
+        SetParameterDescription("outdev", "Per-class standard deviation output CSV");
+        AddParameter(ParameterType_OutputFilename, "outcount", "Class counts");
+        SetParameterDescription("outcount", "Per-class pixel count output CSV");
+
+        AddParameter(ParameterType_StringList, "sp", "Temporal sampling rate");
+        MandatoryOff("sp");
+
+        AddParameter(ParameterType_Choice, "mode", "Mode");
+        SetParameterDescription("mode", "Specifies the choice of output dates (default: resample)");
+        AddChoice("mode.resample", "Specifies the temporal resampling mode");
+        AddChoice("mode.gapfill", "Specifies the gapfilling mode");
+        AddChoice("mode.gapfillmain", "Specifies the gapfilling mode, but only use non-main series "
+                                      "products to fill in the main one");
+        SetParameterString("mode", "resample");
+
+
+        AddParameter(ParameterType_String, "mission",
+                     "The main raster series that will be used. By default, SENTINEL is used");
+        MandatoryOff("mission");
+
+        AddParameter(ParameterType_Bool, "rededge", "Include Sentinel-2 vegetation red edge bands");
+        MandatoryOff("rededge");
+
+        AddRAMParameter();
+
+        SetDocExampleParameterValue("il", "image1.xml image2.xml");
+        SetDocExampleParameterValue("out", "features.tif");
+    }
+
+    void DoUpdateParameters() override {}
+
+    void DoExecute() override
+    {
+        // Get the list of input files
+        const std::vector<std::string> &descriptors = this->GetParameterStringList("il");
+        if (descriptors.size() == 0) {
+            itkExceptionMacro("No input file set...");
+        }
+
+        auto redEdge = GetParameterInt("rededge") != 0;
+        const float pixSize = redEdge ? 20.0f : 10.0f;
+        // get the main mission
+        std::string mission = SENTINEL;
+        if (HasValue("mission")) {
+            mission = this->GetParameterString("mission");
+        }
+
+        TileData td;
+        if (redEdge) {
+            m_RedEdgeBandsExtractor = RedEdgeBandsExtractor::New();
+            m_RedEdgeBandsExtractor->SetPixelSize(pixSize);
+            m_RedEdgeBandsExtractor->SetMission(mission);
+            m_RedEdgeBandsExtractor->SetUseSwir2Band(true);
+
+            m_RedEdgeBandsExtractor->updateRequiredImageSize(descriptors, 0, descriptors.size(),
+                                                             td);
+            m_RedEdgeBandsExtractor->Build(descriptors.begin(), descriptors.end(), td);
+        } else {
+            m_CropTypePreprocessing = CropTypePreprocessing::New();
+            m_CropTypePreprocessing->SetPixelSize(pixSize);
+            m_CropTypePreprocessing->SetMission(mission);
+            m_CropTypePreprocessing->SetUseSwir2Band(true);
+
+            // compute the desired size of the processed rasters
+            m_CropTypePreprocessing->updateRequiredImageSize(descriptors, 0, descriptors.size(),
+                                                             td);
+            m_CropTypePreprocessing->Build(descriptors.begin(), descriptors.end(), td);
+        }
+
+        TemporalResamplingMode resamplingMode = TemporalResamplingMode::Resample;
+        const auto &modeStr = GetParameterString("mode");
+        if (modeStr == "gapfill") {
+            resamplingMode = TemporalResamplingMode::GapFill;
+        } else if (modeStr == "gapfillmain") {
+            resamplingMode = TemporalResamplingMode::GapFillMainMission;
+        }
+
+        std::vector<SensorPreferences> sp;
+        if (HasValue("sp")) {
+            const auto &spValues = GetParameterStringList("sp");
+            sp = parseSensorPreferences(spValues);
+        } else {
+            sp.emplace_back(SensorPreferences{ "SENTINEL", 0, 10 });
+            sp.emplace_back(SensorPreferences{ "SPOT", 1, 5 });
+            sp.emplace_back(SensorPreferences{ "LANDSAT", 2, 16 });
+        }
+
+        FloatVectorImageType *featureImage;
+        if (redEdge) {
+            auto preprocessors = RedEdgeBandsExtractorList::New();
+            preprocessors->PushBack(m_RedEdgeBandsExtractor);
+
+            std::vector<MissionDays> sensorOutDays;
+            if (HasValue("dates")) {
+                sensorOutDays = readOutputDays(GetParameterString("dates"));
+            } else {
+                sensorOutDays = getOutputDays(preprocessors, resamplingMode, mission, sp);
+            }
+
+            if (HasValue("outdates")) {
+                writeOutputDays(sensorOutDays, GetParameterString("outdates"));
+            }
+
+            featureImage = m_RedEdgeBandsExtractor->GetOutput(sensorOutDays);
+        } else {
+            auto preprocessors = CropTypePreprocessingList::New();
+            preprocessors->PushBack(m_CropTypePreprocessing);
+
+            std::vector<MissionDays> sensorOutDays;
+            if (HasValue("dates")) {
+                sensorOutDays = readOutputDays(GetParameterString("dates"));
+            } else {
+                sensorOutDays = getOutputDays(preprocessors, resamplingMode, mission, sp);
+            }
+
+            if (HasValue("outdates")) {
+                writeOutputDays(sensorOutDays, GetParameterString("outdates"));
+            }
+
+            featureImage = m_CropTypePreprocessing->GetOutput(sensorOutDays);
+        }
+        featureImage->UpdateOutputInformation();
+
+        auto classImage = GetParameterInt32Image("ref");
+
+        m_StatisticsFilter = StatisticsFilterType::New();
+        m_StatisticsFilter->SetInput(featureImage);
+        m_StatisticsFilter->SetInputLabelImage(classImage);
+        m_StatisticsFilter->SetNoDataValue(-10000);
+        m_StatisticsFilter->SetUseNoDataValue(true);
+
+        AddProcess(m_StatisticsFilter->GetStreamer(), "Computing features...");
+
+        m_StatisticsFilter->Update();
+
+        const auto &meanValues = m_StatisticsFilter->GetMeanValueMap();
+        const auto &stdDevValues = m_StatisticsFilter->GetStandardDeviationValueMap();
+        const auto &countValues = m_StatisticsFilter->GetPixelCountMap();
+
+        const auto &outmean = GetParameterString("outmean");
+        const auto &outdev = GetParameterString("outdev");
+        const auto &outcount = GetParameterString("outcount");
+
+        std::ofstream fmean(outmean);
+        std::ofstream fdev(outdev);
+        std::ofstream fcount(outcount);
+
+        for (auto it = meanValues.begin(); it != meanValues.end(); ++it) {
+            fmean << it->first;
+            for (unsigned int i = 0; i < it->second.Size(); i++)
+                fmean << ',' << it->second[i];
+            fmean << '\n';
+        }
+        fmean.close();
+        if (!fmean) {
+            itkGenericExceptionMacro("Unable to save " + outmean);
+        }
+
+        for (auto it = stdDevValues.begin(); it != stdDevValues.end(); ++it) {
+            fdev << it->first;
+            for (unsigned int i = 0; i < it->second.Size(); i++)
+                fdev << ',' << it->second[i];
+            fdev << '\n';
+        }
+        fdev.close();
+        if (!fdev) {
+            itkGenericExceptionMacro("Unable to save " + outdev);
+        }
+
+        for (auto it = countValues.begin(); it != countValues.end(); ++it) {
+            fcount << it->first;
+            for (unsigned int i = 0; i < it->second.Size(); i++)
+                fcount << ',' << it->second[i];
+            fcount << '\n';
+        }
+        fcount.close();
+        if (!fcount) {
+            itkGenericExceptionMacro("Unable to save " + outcount);
+        }
+    }
+
+    CropTypePreprocessing::Pointer m_CropTypePreprocessing;
+    RedEdgeBandsExtractor::Pointer m_RedEdgeBandsExtractor;
+    StatisticsFilterType::Pointer m_StatisticsFilter;
+};
+} // namespace Wrapper
+} // namespace otb
+
+OTB_APPLICATION_EXPORT(otb::Wrapper::OpticalFeatures)
